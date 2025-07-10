8eed3514d6f650c7c6c34b8de0978a913dcb4297
NitinVerma07 <nverma404496@gmail.com>
2025-07-10 21:30:20 +0530

    Initial commit

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
+)
 .then(() => console.log("✅ Connected to MongoDB"))
 .catch((err) => console.error("❌ MongoDB connection error:", err));
 
